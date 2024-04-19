c5b89d755bba7fb679192289e80cfd090d625674
Kerubo0 <mmyers@kabarak.com>
2024-04-19 10:33:56 +0300

    edited navbar

--- public/app.js
+++ public/app.js
@@ -55,3 +55,54 @@ const btnFive = document.getElementById("btn-5");
 btnFive.addEventListener("click", ()=> {
  window.location.href = "http://localhost:3000/join"
 })
+
+const angleOne = document.getElementById("angle-one")
+var dropdownMenu = document.getElementById('dropdownMenu');
+
+  // Add click event listener to the image
+  angleOne.onclick = function() {
+    // Toggle the display property
+    if (dropdownMenu.style.display === "none" || dropdownMenu.style.display === "") {
+        dropdownMenu.style.display = "block";
+    } else {
+        dropdownMenu.style.display = "none";
+    }
+};
+
+// Get all elements with the class name 'angle'
+var angleImages = document.querySelectorAll('.angle');
+
+// Add click event listeners to each image
+angleImages.forEach(function(image) {
+    image.addEventListener('click', function() {
+        // Get the corresponding dropdown menu for this image
+        var dropdownMenu = this.parentElement.querySelector('.dropdown');
+
+        // Toggle the display property of the dropdown menu
+        if (dropdownMenu.style.display === "none" || dropdownMenu.style.display === "") {
+            dropdownMenu.style.display = "block";
+            // Add rotation class
+            image.classList.add('rotate180');
+        } else {
+            dropdownMenu.style.display = "none";
+            // Remove rotation class
+            image.classList.remove('rotate180');
+        }
+    });
+});
+
+
+
+var angleImages = document.querySelectorAll('.angle');
+const angleElem = document.getElementById("expand_less")
+ // Get the dropdown menu
+ var dropdownMenu = document.getElementById('dropdownMenu');
+
+  // Add click event listener to the expand less image
+  angleElem.addEventListener('click', function() {
+    // Hide the dropdown menu
+    dropdownMenu.style.display = "none";
+
+    // Show the expand more image
+    //angleImages.style.display = "block";
+});
